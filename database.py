ae3c66495284e9df7b83167eed37445bab22d8db
denzel <denzel.solmayor@calibr8systems.com>
2025-08-19 17:17:02 +0800

    changed db creds

--- database.py
+++ database.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.automap import automap_base
 from typing import AsyncGenerator
 # administrator:helios@helios-configdb:5432/helios
+# administrator:helios@10.147.18.242:5433/helios
 
-DATABASE_URL = "postgresql+asyncpg://administrator:helios@10.147.18.242:5433/helios"
+DATABASE_URL = "postgresql+asyncpg://administrator:helios@helios-configdb:5432/helios"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 AsyncSessionLocal = sessionmaker(
--- main.py
+++ main.py
@@ -29,12 +29,7 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
