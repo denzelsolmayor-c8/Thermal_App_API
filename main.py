ab985ead44384e33b34c8868c7092c189b85483d
denzel <denzel.solmayor@calibr8systems.com>
2025-08-27 14:49:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_origin_regex=".*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
